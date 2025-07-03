38e4ef0ffaa96bfb2c3a6631423c2e0343087bd2
Colour win <bprashik97@gmail.com>
2025-07-03 14:13:46 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.colourwin;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
